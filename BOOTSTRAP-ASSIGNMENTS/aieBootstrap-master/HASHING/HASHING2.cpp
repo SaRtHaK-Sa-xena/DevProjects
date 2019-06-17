36f3af55536efd27aaef940defa8a6ab00502c60
SarthakSaxena <34013565+SarthakSaxena3-AIE@users.noreply.github.com>
2019-06-18 09:38:14 +1000

    Untrustworthy Commit for Hashing
    Hashing has been fixed but be careful, it might have errors.

---
+++ BOOTSTRAP-ASSIGNMENTS/aieBootstrap-master/HASHING/HASHING2.cpp
@@ -0,0 +1,115 @@
+// HASHING.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include "Hash2.h"
+
+#include <string>
+using namespace std;
+
+int main()
+{
+	
+	hashClass Hashuse;
+	string name = "";
+	string book = "";
+	int ID = 0;
+	bool cont = true;
+	char choice = '0';
+	string options;
+
+
+
+
+	while (cont)
+	{	
+		cout << "------(a)dd-------(r)emove-------(f)ind----(p)rint---(e)xit---" << endl;
+		cin >> choice;
+
+		switch (choice)
+		{
+		case'a':
+			cout << "\t\t\t adding..." << endl;
+			cout << "Enter Name: " << endl;
+			cin >> name;
+			cout << "Enter Favourite Book: " << endl;
+			cin >> book;
+			cout << "Enter Unique id: " << endl;
+			cin >> ID;
+			Hashuse.AddItem(name, book, ID);
+			break;
+		case'r':
+			cout << "\t\t\t removing..." << endl;
+			cout << "Find Person: " << endl;
+			cin >> name;
+			Hashuse.RemoveItem(name);
+			break;
+
+
+		case'f':
+			cout << "\t\t\t Finding..." << endl;
+			cout << "find (book) or (id)" << endl;
+			cin >> options;
+			if (options == "book")
+			{
+				cout << "Enter name to search: " << endl;
+				cin >> name;
+				cout << "Finding Book Name..." << endl;
+				cout << endl;
+				cout << "===========================" << endl;
+				Hashuse.FindBook(name);
+				cout << "===========================" << endl;
+				break;
+			}
+			else if (options == "id")
+			{
+				cout << "Enter name to search" << endl;
+				cin >> name;
+				cout << "Finding their unique id..." << endl;
+				cout << endl;
+				cout << "===========================" << endl;
+				Hashuse.FindID(name);
+				cout << "===========================" << endl;
+				break;
+			}
+			else
+			{
+				cout << "\t\t\t Wrong input...\n \t\t\t try again later..." << endl;
+				break;
+			}
+			break;
+
+
+		case'p':
+			cout << "\t\t\t Printing" << endl;
+			Hashuse.PrintTable();
+			break;
+
+		case'e':
+			return 0;
+		default:
+			cout << "Pick one of the options listed" << endl;
+		}
+	}
+
+	//===============
+	/*while (name != "exit")
+	{
+		cout << "Remove ";
+		cin >> name;
+		if (name != "exit")
+		{
+			Hashy.RemoveItem(name);
+		}
+	}*/
+	//================
+	//Hashy.PrintItemsInIndex(2);
+
+	//Hashy.PrintItemsInIndex(8);
+	//Hashy
+
+	/*int index;
+	hashClass hashObject;
+	index = hashObject.Hash("Paula");
+	cout << "Index = " << index << endl;*/
+}
+
+
---
+++ BOOTSTRAP-ASSIGNMENTS/aieBootstrap-master/HASHING/Hash2.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class hashClass
+{
+public:
+	hashClass();
+
+	int Hash(string key); //location to find
+
+	void AddItem(string name, string book, int ID); //items to be added
+
+	int NumberOfItemsInIndex(int index); // for displaying items
+
+	void PrintTable(); //displays entire list
+
+	void PrintItemsInIndex(int index); // displays in index
+
+	void FindBook(string name); // finds by name--> displays book
+
+	void FindID(string name); // finds by name--> displays ID
+
+
+	void RemoveItem(string name); // removes key, therefore removing the index's values
+
+private:
+	static const int tableSize = 4;
+
+	struct item
+	{
+		string name;
+		string favBookName;
+		int itemCode;
+		item* next;
+	};
+
+	item* HashTable[tableSize];
+};
