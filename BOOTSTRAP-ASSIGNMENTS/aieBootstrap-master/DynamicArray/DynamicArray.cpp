935e836e0c8458f186e6f7cc40ae0a5ab20e9096
SaRtHaK-Sa-xena <34013565+SaRtHaK-Sa-xena@users.noreply.github.com>
2019-06-17 16:55:44 +1000

    End Of Day
    6/17/2019
    4:55 PM
    finished Dynamic Array

--- BOOTSTRAP-ASSIGNMENTS/aieBootstrap-master/DynamicArray/DynamicArray.cpp
+++ BOOTSTRAP-ASSIGNMENTS/aieBootstrap-master/DynamicArray/DynamicArray.cpp
@@ -17,13 +17,14 @@ int main()
 	int i = 0;
 	int t1 = 555;
 	int t2 = 222;
+	int searchId;
 	NodeClass* current;
 	NodeClass* currentRight;
 	
 	while (cont)
 	{
 		cout << "What Do you want to Do? " << endl;
-		cout << "---(a)dd---(d)isplay---(s)ort---(e)xit---" << endl;
+		cout << "---(a)dd---(d)isplay---(s)ort--(f)ind--(e)xit---" << endl;
 		cin >> choice;
 		switch (choice)
 		{
@@ -98,6 +99,12 @@ int main()
 			i = 0;
 			//==============DISPLAY====================
 
+			break;
+		case'f':
+			cout << "\t\t\t searching..." << endl;
+			cout << "Enter Id To Look For: " << endl;
+			cin >> searchId;
+			DynamicTree->search(searchId);
 			break;
 		case'e':
 			cout << "Exiting..." << endl;
