f8924145e8bbf5fe72bb0a251bbdd12534182028
SarthakSaxena <34013565+SarthakSaxena3-AIE@users.noreply.github.com>
2019-06-26 15:28:53 +1000

    Before 3:30 Break
    Added a ton.
    Made setBit, checkBit, and bitfield functions.
    Got The Peer Review for Tank Game

--- UnitTest/Binary/Binary.cpp
+++ UnitTest/Binary/Binary.cpp
@@ -9,22 +9,26 @@
 
 using namespace std;
 
-const char BinaryData = 11011;
+const char BinaryData[] = { "11011" };
+char BinaryNumber[8];
 
-const char CHAINSAW = 0x01;
-const char PISTOL = 0x01 << 1;
-const char SHOTGUN = 0x01 << 2;
-const char SUPER_SHOTGUN = 0x01 << 3;
-const char CHAINGUN = 0x01 << 4;
-const char ROCKET_LAUNCHER = 0x01 << 5;
-const char PLASMA_GUN = 0x01 << 6;
-const char BFG9000 = 0x01 << 7;
+//const char MACHETE = 0;
+//const char CHAINSAW = 0x01;
+//const char PISTOL = 0x01 << 1;
+//const char SHOTGUN = 0x01 << 2;
+//const char SUPER_SHOTGUN = 0x01 << 3;
+//const char CHAINGUN = 0x01 << 4;
+//const char ROCKET_LAUNCHER = 0x01 << 5;
+//const char PLASMA_GUN = 0x01 << 6;
+//const char BFG9000 = 0x01 << 7;
+//
+//const int WEAPONS_COUNT = 10;
+//
+//const char* weapons[] = { "Fists","Chainsaw", "Pistol", "Shotgun", "Super Shotgun", "Chaingun", "Rocket Launcher", "Plasma Gun", "BFG 9000"};
 
-const int WEAPONS_COUNT = 9;
+//const char weapon[] = { MACHETE, CHAINSAW, PISTOL, SHOTGUN, SUPER_SHOTGUN, CHAINGUN, ROCKET_LAUNCHER, PLASMA_GUN, BFG9000 };
 
-const char* weapons[] = { "Fists","Chainsaw", "Pistol", "Shotgun", "Super Shotgun", "Chaingun", "Rocket Launcher", "Plasma Gun", "BFG 9000" };
-
-void printInventoy(char inventory);
+//void printInventoy(char inventory);
 void addToInventory(char& inventory, char weapon);
 
 int main()
@@ -35,15 +39,22 @@ int main()
 	string variable;
 	bool cont = true;
 	char weapon[20];
-	char weapon2;
+
 
 	int bitArray[50];
 	int bitNo;
 	int Decimal;
 	int temp;
+	int i;
 
-	weapons[rand() % 10];
+	//========Intialization=========
+	const char CHAINSAW = 1 << 2;
+	const char PISTOL = 1 << 3;
+	const char CHAINGUN = 1 << 4;
+	int n_number = 0;
+	//========Intialization=========
 
+	//weapons[rand() % 10];
 	while (cont)
 	{
 		cout << "(a)dd----(d)isplay--(b)itfield_Example----" << endl;
@@ -51,48 +62,125 @@ int main()
 		switch (choice)
 		{
 		    case'a':
-		    	cout << "\t\t\t adding..." << endl;
+		    	cout << "\t\t\t testing..." << endl;
+
+									/*TEST*/
+				//manually add Chainsaw, Pistol, Chaingun to inventory
+				inventory |= CHAINSAW; //set to 00000100
+				inventory |= PISTOL; //set to 00010000
+				inventory |= CHAINGUN; //set to 00001000
+				//====================================================
+
+				
+				//add through Setbit function, First check if it has
+				//been set, then return set. After that check if it has
+				//been set and clear it, then check if it has been clea-
+				//-red.
+				cout << "----Enter to clear[0] or set[1]----" << endl;
+				cin >> n_number;
+				setBit(inventory, 2, 0); //this is to check if CHAINSAW
+										 //is set, so if set to 1 it'll
+										 //keep the value.
+				if (checkBit(inventory, 2) == '1')
+				{
+					cout << "CHAINSAW set" << endl;
+				}
+				else
+				{
+					cout << "CHAINSAW not set" << endl;
+				}
+				//=====================================================
+
+				system("PAUSE");
+				cout << "========================================" << endl;
+				//Use checkbit to see if that value is set to '1' or '0
+				if (checkBit(inventory, 2) == '1')
+				{
+					cout << "Check Passed: " << "The Position was set" << endl;
+					cout << "CHAINSAW has been set in inventory" << endl;
+				}
+				else
+				{
+					cout << "CHAINSAW WAS NOT SET" << endl;
+				}
+				cout << "========================================" << endl;
+				if (checkBit(inventory, 3) == '1')
+				{
+					cout << "Check Passed: " << "The Position was set" << endl;
+					cout << "PISTOL has been set in inventory" << endl;
+				}
+				else
+				{
+					cout << "PISTOL WAS NOT SET" << endl;
+				}
+				cout << "========================================" << endl;
+				if (checkBit(inventory, 4) == '1')
+				{
+					cout << "Check Passed: " << "The Position was set" << endl;
+					cout << "CHAINGUN has been set in inventory" << endl;
+				}
+				else
+				{
+					cout << "CHAINGUN WAS NOT SET" << endl;
+				}
+				//=====================================================
+
+				system("PAUSE");
+
+
+
+
 		    	cout << "Enter Gun Name: " << endl;
-		    	
 				cin >> variable;
-				if (variable == "Chainsaw")
-				{
-					addToInventory(inventory, CHAINSAW);
-				}
-				else if (variable == "Chaingun")
-				{
-					addToInventory(inventory, CHAINGUN);
-				}
-				else if (variable == "Pistol")
-				{
-					addToInventory(inventory, PISTOL);
-				}
-				else if (variable == "Shotgun")
-				{
-					addToInventory(inventory, SHOTGUN);
-				}
-				else if (variable == "Super Shotgun")
-				{
-					addToInventory(inventory, SUPER_SHOTGUN);
-				}
-				else if (variable == "Rocket Launcher")
-				{
-					addToInventory(inventory, ROCKET_LAUNCHER);
-				}
-				else if (variable == "Plasma Gun")
-				{
-					addToInventory(inventory, PLASMA_GUN);
-				}
-				else if (variable == "BFG9000")
-				{
-					addToInventory(inventory, BFG9000);
-				}
-		    	//addToInventory(inventory, drw);
+
+				//cout << "Enter Item No: " << endl;
+				//cin >> temp;
+
+				//cout << "Which Weapon" << endl;
+				//cin >> i;
+				//setBit(inventory, weapon[i], temp);
+
+				//cin >> variable;
+				//if (variable == "Chainsaw")
+				//{
+				//	if ((inventory & CHAINSAW) == CHAINSAW)
+				//	{
+				//		cout << "Yes, True" << endl;
+				//	}
+				//}
+				//else if (variable == "Chaingun")
+				//{
+				//	addToInventory(inventory, CHAINGUN);
+				//}
+				//else if (variable == "Pistol")
+				//{
+				//	addToInventory(inventory, PISTOL);
+				//}
+				//else if (variable == "Shotgun")
+				//{
+				//	addToInventory(inventory, SHOTGUN);
+				//}
+				//else if (variable == "Super Shotgun")
+				//{
+				//	addToInventory(inventory, SUPER_SHOTGUN);
+				//}
+				//else if (variable == "Rocket Launcher")
+				//{
+				//	addToInventory(inventory, ROCKET_LAUNCHER);
+				//}
+				//else if (variable == "Plasma Gun")
+				//{
+				//	addToInventory(inventory, PLASMA_GUN);
+				//}
+				//else if (variable == "BFG9000")
+				//{
+				//	addToInventory(inventory, BFG9000);
+				//}
 		    	break;
 		    case'd':
 		    	cout << "\t\t\t displaying..." << endl;
 		    	cout << "=========Display============" << endl;
-		    	printInventoy(inventory);
+		    	//printInventoy(inventory);
 				break;
 			case'e':
 				return 0;
@@ -105,25 +193,9 @@ int main()
 				cin >> variable;
 				if (variable == "b")
 				{
-					//cout << "Enter size of binary data" << endl;
-					//cin >> temp;
-
-					//cout << "Enter the binary of your choice" << endl;
-					//for (int i = 0; i < temp; i++)
-					//{
-						//cout << "Enter First Binary Number: " << i + 1 << endl;
-						//cin >> bitArray[i];
-					//}
-					cout << "Result: " << binToDec(&BinaryData);
-					//cout << "Converting..." << endl;
-					////convert function
-					////binary-->Decimal
-					//cout << "Decimal: " << endl;
-					//cout << BinaryToDecimal(bitArray, temp) << endl;
-					//cin.get();
-
-					//cout << "Finished Converting..." << endl;
-
+					cout << "Result: " << binToDec(BinaryData);
+					//convert function
+					//binary-->Decimal
 				}
 				else if (variable == "d")
 				{
@@ -131,6 +203,7 @@ int main()
 					// [1]--> temp = decimal number to be inputted
 					cout << "Enter the Decimal of your choice" << endl;
 					cin >> temp;
+					decToBin(BinaryNumber, 8, temp);
 					cout << "Converting..." << endl;
 					//convert function
 					//Decimal-->binary
@@ -138,7 +211,8 @@ int main()
 
 					cin.get();
 					cout << "Binary: " << endl;
-					DecimalToBinary(temp);
+					cout << BinaryNumber;
+					//DecimalToBinary(temp);
 
 				}
 				break;
@@ -156,19 +230,19 @@ int main()
 	cin.get();
 }
 
-void printInventoy(char inventory)
-{
-	cout << weapons[0] << " | ";
-	for (int i = 1; i < WEAPONS_COUNT; i++)
-	{
-		char mask = 0x01 << i - 1;
-		if ((inventory & mask) == mask)
-		{
-			cout << weapons[i] << " | ";
-		}
-	}
-	cout << endl;
-}
+//void printInventoy(char inventory)
+//{
+//	cout << weapons[0] << " | ";
+//	for (int i = 1; i < WEAPONS_COUNT; i++)
+//	{
+//		char mask = 0x01 << i - 1;
+//		if ((inventory & mask) == mask)
+//		{
+//			cout << weapons[i] << " | ";
+//		}
+//	}
+//	cout << endl;
+//}
 
 void addToInventory(char& inventory, char weapon)
 {
--- UnitTest/Binary/BitfieldOperators.h
+++ UnitTest/Binary/BitfieldOperators.h
@@ -29,7 +29,6 @@ int BinaryToDecimal(int Array[], int size)
 
 	return result;
 }
-
 void DecimalToBinary(int input)
 {
 	int decrementer = 128;
@@ -55,23 +54,98 @@ int binToDec(const char* binaryString)
 {
 	int result = 0;
 
-	for (int i = 0; i < 5; i++)
+	for (int i = 4; i > -1; i--) //starting iterator at the end of the array
 	{
-		result = result << 1 | 5 + binaryString[i];
+		if (binaryString[i] == '1')
+		{
+			result = result | 1 << 4 /*maxSize*/ - i; //size puts it at the end of the array, then end - 1, so it always is furthest right
+			//result = result << 1 | 5 + binaryString[i];
+		}
+		//else--> number is 0, so it shouldn't get result to calculate it
 	}
-	return result;
+	return result; //returns correct decimal
 }
+
 void decToBin(char* binaryString, int length, int value)
 {
-
+	int decrementer = 128;
+	for (int i = 0; i < length; i++)//iterate over as many numbers as there are in array-->(8)
+	{
+		if (value >= decrementer)
+		{
+			value = value - decrementer;//TRUE then subtract
+			binaryString[i] = '1'; //since true, write 1 inside binaryString
+		}
+		else
+		{
+			binaryString[i] = '0';//FALSE can't fit inside it so place 0
+		}
+		decrementer = decrementer / 2;//then after return true or false ---> make decrementer move down the array by dividing it by 2
+																 //Array --->  [128][64][32][16][8][4][2][1]  <----- Array
+	}
 }
 void setBit(char& bitfield, char bit, char value)
 {
-	
+	//bitfield = bitfield | value << bit;
+
+	//cout << bitfield << " | ";
+	//for (int i = 1; i < WEAPONS_COUNT; i++)
+	//{
+	//	char mask = 0x01 << i - 1;
+	//	if ((inventory & mask) == mask)
+	//	{
+	//		cout << weapons[i] << " | ";
+	//	}
+	//}
+	//cout << endl;
+
+
+	//1 << bit;
+
+	//char mask = 0x01 << value;
+
+	//if ((bitfield & mask) == mask)
+	//{
+	//	cout << "Set!" << endl;
+	//}
+	//else
+	//{
+	//	bitfield ^= value;
+	//}
+
+
+
+
+
+
+
+
+	//create temporary char to create a bit in that place, for being created or being cleared
+	// 0 = cleared, 1 = set
+
+	char temporary = 1 << bit; //moves it to the intended bit
+	if (value == 1) //if we are setting it
+	{
+		bitfield |= temporary; //sets that position to equal 1.
+	}
+	else //if we are clearing it, so 0
+	{
+		bitfield ^= temporary; //sets that position to equal 0.
+	}
+
 }
 char checkBit(char& bitfield, char bit)
 {
-	return 'c';
+	//make temporary char to return the position of variable in field
+	char temporary = 1 << bit; /*<-- This shifts it by "bit" and moves*/
+	if ((bitfield & temporary) == temporary) /*To the variable we want to use.*/
+	{
+		return '1'; //if 1 then it has been set
+	}
+	else
+	{
+		return '0'; //if 0 then hasn't been set
+	}
 }
 
 
