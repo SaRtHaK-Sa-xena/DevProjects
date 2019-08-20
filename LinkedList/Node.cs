9b386c41c6a9b53babc29bf3308f3169665e1676
SarthakSaxena <34013565+SarthakSaxena3-AIE@users.noreply.github.com>
2019-08-20 15:58:33 +1000

    Saving Progress.
    Saving Linked List

--- LinkedList/Node.cs
+++ LinkedList/Node.cs
@@ -6,50 +6,50 @@ using System.Threading.Tasks;
 
 namespace LinkedList
 {
-    class Node : LinkedList
-    {
-        //public void Node()
+    //class Node : LinkedList
+    //{
+    //    //public void Node()
 
-        public Node()
-        {
-            next = null;
-            m_data = 0;
-        }
+    //    public Node()
+    //    {
+    //        next = null;
+    //        m_data = 0;
+    //    }
 
-        public Node(int a_data)
-        {
-            m_data = a_data;
-        }
+    //    public Node(int a_data)
+    //    {
+    //        m_data = a_data;
+    //    }
 
-        //sets data
-        public void SetNodeData(int a_data)
-        {
-            //set data
-            m_data = a_data;
-        }
-        //Returns data
-        public int GetNodeData()
-        {
-            return m_data;
-        }
+    //    //sets data
+    //    public void SetNodeData(int a_data)
+    //    {
+    //        //set data
+    //        m_data = a_data;
+    //    }
+    //    //Returns data
+    //    public int GetNodeData()
+    //    {
+    //        return m_data;
+    //    }
 
-        //get next 
-        public Node GetRight()
-        {
-            //returns next of current node
-            return this.next;
-        }
+    //    //get next 
+    //    public Node GetRight()
+    //    {
+    //        //returns next of current node
+    //        return this.next;
+    //    }
 
-        //set next 
-        public void SetRight(Node a_next)
-        {
-            this.next = a_next;
-        }
+    //    //set next 
+    //    public void SetRight(Node a_next)
+    //    {
+    //        this.next = a_next;
+    //    }
 
-        private int m_data;
-        private Node next = new Node();
+    //    private int m_data;
+    //    private Node next = new Node();
 
-        //store value
-        //store right and left
-    }
+    //    //store value
+    //    //store right and left
+    //}
 }
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -21,7 +21,7 @@ namespace LinkedList
                 {
                     case 1:
                         Console.Write("\tAdd...");
-                        
+
                         //add function
                         break;
                     case 2:
@@ -45,13 +45,38 @@ namespace LinkedList
         }
     }
 
-    class LinkedList
+    class LinkedList : Node
     {
+
+        public void PushFront(int a_value)
+        {
+            Node node = new Node();
+
+
+        }
+
+
+
         public void Print()
         {
-            if(ref Empty == false)
+            if (!Empty())
             {
-
+                //. instead of * 
+                Node* head = this->head;
+                ofType(Node)
+                head
+                
+                int i = 1;
+                while (head != null)
+                {
+                    Console.WriteLine(i + ": " + head.GetNodeData() + "\n"); //writes out list in console
+                    head = head.GetRight(); //iterate towards next
+                    i++;
+                }
+            }
+            else //list empty
+            {
+                Console.WriteLine("List is already Empty..." + "\n");
             }
         }
 
@@ -76,5 +101,52 @@ namespace LinkedList
 
         private Node head, tail;
         int m_size;
+    }
+
+    class Node
+    {
+        //public void Node()
+
+        public Node()
+        {
+            next = null;
+            m_data = 0;
+        }
+
+        public Node(int a_data)
+        {
+            m_data = a_data;
+        }
+
+        //sets data
+        public void SetNodeData(int a_data)
+        {
+            //set data
+            m_data = a_data;
+        }
+        //Returns data
+        public int GetNodeData()
+        {
+            return m_data;
+        }
+
+        //get next 
+        public Node GetRight()
+        {
+            //returns next of current node
+            return this.next;
+        }
+
+        //set next 
+        public void SetRight(Node a_next)
+        {
+            this.next = a_next;
+        }
+
+        public int m_data;
+        public Node next = new Node();
+
+        //store value
+        //store right and left
     }
 }
