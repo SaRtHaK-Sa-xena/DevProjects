3401f3685571b9c6a7a74f9aed8dffbb30dd4071
SarthakSaxena <34013565+SarthakSaxena3-AIE@users.noreply.github.com>
2019-08-20 11:59:39 +1000

    Saving Changes Before Lunch
    Saving Progress. Need to include bootstrap link in TDD. Doing it right now.

---
+++ RPC Client/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Runtime.Remoting.Channels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPC
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 8888;
+            string playerName = "Player";
+            TcpClientChannel channel = new TcpClientChannel();
+            ChannelServices.RegisterChannel(channel, false); //boolean value to see if it should be encrypted or not
+
+            //Get a reference of Player object from Server
+            string playerURL = "tcp://localhost:" + port + "/" + playerName;
+            Player player = (Player)Activator.GetObject(typeof(Player), playerURL);
+
+            //loop
+            while (true)
+            {
+                Console.Write("Type a message to the server or type 'quit' to exit\n");
+                string text = Console.ReadLine();
+
+                if (text == "quit")
+                {
+                    break;
+                }
+                //RPC: Call Function on server
+                player.GetInput(text); //getInput
+
+                
+            }
+        }
+    }
+
+
+
+    class Player : MarshalByRefObject
+    {
+        //functions to call remotely
+        public void GetInput(string text)
+        {
+            //plus adds to the line like "<<"
+            Console.WriteLine("The client says: " + text);
+
+            if (text == "getTime")
+            {
+                Console.WriteLine("Server: Current Time ");
+                Console.WriteLine(DateTime.Now.ToString("h:mm:ss tt"));
+            }
+
+            if (text.StartsWith("t" + "h" + "a" + "n" + "k") || text == "thank you server")
+            {
+                Console.WriteLine("Server: Your Welcome");
+            }
+        }
+    }
+
+}
---
+++ RPC Server/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Runtime.Remoting.Channels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 8888; //just a random port, that isn't being used by anyone
+            TcpServerChannel channel = new TcpServerChannel(port); //of port
+            ChannelServices.RegisterChannel(channel, false); //boolean value to see if it should be encrypted or not
+
+            //Register Player                    //type of class we register, name for that class, whether processed by new object or singleton
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(Player), "Player", WellKnownObjectMode.SingleCall);
+
+            Console.WriteLine("Listening for requests. \nPress enter to exit...\n");
+            Console.ReadLine();
+        }
+    }
+
+    class Player : MarshalByRefObject
+    {
+        //functions to call remotely
+        public void GetInput(string text)
+        {
+            Console.WriteLine("The client says: " + text);
+
+            //returns time
+            if (text == "getTime")
+            {
+                Console.WriteLine("Server: Current Time ");
+                Console.WriteLine(DateTime.Now.ToString("h:mm:ss tt"));
+            }
+
+            //returns your welcome makes it more intuitive
+            if (text.StartsWith("t" + "h" + "a" + "n" + "k") || text == "thank you server")
+            {
+                Console.WriteLine("Server: Your Welcome");
+            }
+        }
+    }
+}
