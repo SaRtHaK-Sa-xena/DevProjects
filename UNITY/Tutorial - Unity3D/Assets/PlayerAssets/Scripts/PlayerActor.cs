49f66b53ad033c74dac28d6ed3e6625dbaf8c4c6
SarthakSaxena <34013565+SarthakSaxena3-AIE@users.noreply.github.com>
2019-09-03 11:35:18 +1000

    Saving Changes
    Added Unity in GitHub.
    Starting working on Github now.

---
+++ UNITY/Tutorial - Unity3D/Assets/PlayerAssets/Scripts/PlayerActor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActor : MonoBehaviour {
+
+    private CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+        controller = gameObject.GetComponent<CharacterController>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move_direction = new Vector3(1, 0, 0);
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            move_direction.Set(0, 0, -1);
+            controller.Move(move_direction * Time.deltaTime * 15);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            move_direction.Set(0, 0, 1);
+            controller.Move(move_direction * Time.deltaTime * 15);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            move_direction.Set(-1, 0, 0);
+            controller.Move(move_direction * Time.deltaTime * 15);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            move_direction.Set(1, 0, 0);
+            controller.Move(move_direction * Time.deltaTime * 15);
+        }
+    }
+}
