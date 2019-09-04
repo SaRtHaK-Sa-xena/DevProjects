117eade47c87b329bc4a22c53bf1438e38a3d581
SarthakSaxena <34013565+SarthakSaxena3-AIE@users.noreply.github.com>
2019-09-04 11:48:33 +1000

    Saving Changes.
    Created New Project. Added in Components for VR Oculus. Added in a set up of needed projects.

---
+++ UNITY/NewVRProject/Assets/EnvironmentAssets/Scripts/baloonActor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class baloonActor : MonoBehaviour
+{
+
+    public float speed;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.up * speed);
+    }
+}
