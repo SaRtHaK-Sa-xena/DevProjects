b1aa5081631c6546e62beb21681f440d5fb0b09b
Sarthak Saxena <34013565+SaRtHaK-Sa-xena@users.noreply.github.com>
2019-09-14 20:38:41 +1000

    Saving Changed
    Completed Two versions of Agent AI. FIxed a bit of Unity.

---
+++ UNITY/NewUnityVR/Assets/Scripts/DestroyAnim.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAnim : MonoBehaviour
+{
+    public float minRange;
+    public float maxRange;
+    public GameObject explodeAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Explode();
+    }
+
+    // Update is called once per frame
+    private void Explode()
+    {
+
+
+        foreach(Transform t in transform)
+        {
+            rb = getCompo
+        }
+    }
+}
