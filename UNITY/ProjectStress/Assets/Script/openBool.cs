cd79d27a26221e417361720cef1aed87b9d6e4b1
SaRtHaK-Sa-xena <34013565+SaRtHaK-Sa-xena@users.noreply.github.com>
2019-09-29 21:54:21 +1000

    Saving Changes
    Created Environment, Created basic Player Controller. Need to fix in animation.

---
+++ UNITY/ProjectStress/Assets/Script/openBool.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openBool : MonoBehaviour
+{
+
+    [SerializeField]
+    private Animator animationController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            animationController.SetBool("openDoors", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            animationController.SetBool("openDoors", false);
+        }
+    }
+}
---
+++ UNITY/ProjectStress/Assets/openDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openDoor : MonoBehaviour
+{
+
+    private GameObject PlayerContainer;
+
+    [SerializeField]
+    private Animator myAnimationController;
+
+    private void Start()
+    {
+        PlayerContainer = GameObject.Find("Player");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            myAnimationController.SetBool("openDoors", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            myAnimationController.SetBool("openDoors", false);
+        }
+    }
+}
