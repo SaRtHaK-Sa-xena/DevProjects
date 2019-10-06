b03bc11155ecb655d20232ff88c17f19b7251c50
SaRtHaK-Sa-xena <34013565+SaRtHaK-Sa-xena@users.noreply.github.com>
2019-10-06 22:47:58 +1100

    Saving Back up
    The version includes stress meter, interactable item, and mood lighting.

---
+++ UNITY/ProjectStress/Assets/gameEngine.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameEngine : MonoBehaviour {
+
+    private GameObject slider;
+    private int deltaTime = 0;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        deltaTime = deltaTime + 1;
+
+        slider = GameObject.Find("StressMeter");
+        slider.GetComponent<Slider>().value = deltaTime;
+	}
+}
---
+++ UNITY/ProjectStress/Assets/stressMeter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class stressMeter : MonoBehaviour
+{
+
+    private GameObject paperObj1;
+    private GameObject UIStress;
+    private Transform eyes;
+
+    // Use this for initialization
+    void Start()
+    {
+        paperObj1 = GameObject.FindGameObjectWithTag("Good");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        eyes = GameObject.Find("Camera").transform;
+
+        RaycastHit detectHit;
+        if (Physics.Raycast(eyes.position, eyes.forward, out detectHit))
+        {
+            //if detected hit equals to Target
+            if (detectHit.collider.gameObject.CompareTag("Good"))
+            {
+                //Decrease Stress
+                UIStress = GameObject.Find("StressMeter");
+                UIStress.GetComponent<Slider>().value = UIStress.GetComponent<Slider>().value - 10;
+            }
+        }
+    }
+}
