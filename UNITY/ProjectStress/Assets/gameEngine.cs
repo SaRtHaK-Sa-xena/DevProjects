bae260dc50f90d293d36aa2fcc3e8bedb3104ee2
SaRtHaK-Sa-xena <34013565+SaRtHaK-Sa-xena@users.noreply.github.com>
2019-10-07 23:10:21 +1100

    Saving Progress
    Saving halfway done project

--- UNITY/ProjectStress/Assets/gameEngine.cs
+++ UNITY/ProjectStress/Assets/gameEngine.cs
@@ -8,14 +8,28 @@ public class gameEngine : MonoBehaviour {
     private GameObject slider;
     public float deltaTime = 0;
 
+    private GameObject[] platforms;
+
     public bool startStress = true;
 
-	// Update is called once per frame
-	void Update ()
+    private void Start()
+    {
+        platforms = GameObject.FindGameObjectsWithTag("platform");
+        for (int i = 0; i < platforms.Length; i++)
+        {
+            platforms[i].AddComponent<Rigidbody>().useGravity = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
         if(deltaTime > 100)
         {
-           
+            for (int i = 0; i < platforms.Length; i++)
+            { 
+                platforms[i].GetComponent<Rigidbody>().AddForce(Vector3.up);
+            }
         }
 
         if (startStress)
