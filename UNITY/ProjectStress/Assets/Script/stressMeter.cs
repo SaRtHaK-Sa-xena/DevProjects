8a86735aec95eba4530f66c8cfc9feb5d2011d4d
SaRtHaK-Sa-xena <34013565+SaRtHaK-Sa-xena@users.noreply.github.com>
2019-10-08 23:13:17 +1100

    Fixed Game
    There was a bit of issue with the project. Installed HUB and download 3.8 version. Aswell as fixed the GUI text with better project now. Created a copy of the Project as a backup.

---
+++ UNITY/ProjectStress/Assets/Script/stressMeter.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class stressMeter : MonoBehaviour
+{
+
+    private GameObject paperObj1;
+    private GameObject UIStress;
+    private GameObject startButton;
+    private Transform eyes;
+    private GameObject gameEngine;
+    private bool guiText = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        paperObj1 = GameObject.FindGameObjectWithTag("Good");
+        startButton = GameObject.Find("StartButton");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameEngine = GameObject.Find("GameEngine");
+        eyes = GameObject.Find("Camera").transform;
+
+        RaycastHit detectHit;
+        if (Physics.Raycast(eyes.position, eyes.forward, out detectHit))
+        {
+            //if detected hit equals to Target
+            if (detectHit.collider.gameObject.CompareTag("Good"))
+            {
+                //Decrease Stress
+                UIStress = GameObject.Find("StressMeter");
+                UIStress.GetComponent<Slider>().value = UIStress.GetComponent<Slider>().value - 5;
+                gameEngine.GetComponent<gameEngine>().deltaTime = UIStress.GetComponent<Slider>().value;
+                gameEngine.GetComponent<gameEngine>().startStress = false;
+            }
+            if (detectHit.collider.gameObject == startButton)
+            {
+                //show GUI
+                guiText = true;
+            }
+            else
+            {
+                gameEngine.GetComponent<gameEngine>().startStress = true;
+                guiText = false;
+            }
+        }
+
+
+    }
+
+    private void OnGUI()
+    {
+        if(guiText)
+        {
+           GUI.Box(new Rect(300, 300, 200, 20), "Press To Continue");
+        }
+    }
+}
