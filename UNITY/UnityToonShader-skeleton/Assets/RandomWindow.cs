4a125314601d46caca28d0ce53826de3e7ab9462
SaRtHaK-Sa-xena <34013565+SaRtHaK-Sa-xena@users.noreply.github.com>
2019-10-10 00:02:46 +1100

    Making Changes
    Making Changes To Save.

---
+++ UNITY/UnityToonShader-skeleton/Assets/RandomWindow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomWindow : MonoBehaviour
+{
+    private GameObject[] windows;
+    private bool timerStart = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Generate List From Windows
+        windows = GameObject.FindGameObjectsWithTag("windows");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerStart)
+        {
+            //Find random from range of min and max
+            //each update
+            int randomValue = Random.Range(0, windows.Length);
+            windows[randomValue].GetComponentInChildren<Material>().color = Color.red;
+
+            timerStart = true;
+        }
+    }
+}
