a78da01711dd55e7ab2187e1f5dd280314c7d5d8
SaRtHaK-Sa-xena <34013565+SaRtHaK-Sa-xena@users.noreply.github.com>
2019-10-11 23:52:56 +1100

    Fixed All lot of things
    This has a working Random WIndow System. WIth clear concise code with comments. Need to create movement with realist yet cartoony jump and gravity features.

---
+++ UNITY/UnityToonShader-skeleton/Assets/testingScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testingScript : MonoBehaviour
+{
+    private GameObject foundObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foundObj = GameObject.FindGameObjectWithTag("purp");
+
+        foundObj.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
