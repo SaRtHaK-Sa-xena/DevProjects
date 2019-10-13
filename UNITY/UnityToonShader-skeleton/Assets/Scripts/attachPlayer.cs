7c9382c9b53cd2975c397faa3901d304af5a68b4
SaRtHaK-Sa-xena <34013565+SaRtHaK-Sa-xena@users.noreply.github.com>
2019-10-13 22:51:30 +1100

    Saving Changes
    Sort of good. Example of Sample Game

---
+++ UNITY/UnityToonShader-skeleton/Assets/Scripts/attachPlayer.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attachPlayer : MonoBehaviour
+{
+    public Animator ledgeChangeAnim;
+
+    public GameObject playerGameObj;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //start rotation
+        ledgeChangeAnim.enabled = true;
+
+        //parent player to rotating obj
+        other.transform.parent = this.transform;
+
+        playerGameObj.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
+        playerGameObj.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    transform.DetachChildren();
+    //}
+
+    private void Start()
+    {
+        //start with no animation
+        ledgeChangeAnim.enabled = false;
+
+        //Find Player
+        playerGameObj = GameObject.FindGameObjectWithTag("Player");
+    }
+
+
+    private void Update()
+    {
+        float time = 2;
+        if(ledgeChangeAnim.enabled == true)
+        {
+            time = time - 1;
+            if(time <= 0)
+            {
+                Debug.Log("Entered Time Loop");
+                playerGameObj.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+
+                //stop rotation
+                ledgeChangeAnim.enabled = false;
+
+                //unparent player
+                transform.DetachChildren();
+
+                //reset time
+                time = 5;
+            }
+        }
+    }
+}
+
+
+//private void OnTriggerEnter(Collider other)
+//{
+//    //If player enter collider
+//    if (other.gameObject.tag == "Player")
+//    {
+//        ledgeChangeAnim.enabled = true;
+//        other.transform.parent = this.transform;
+
+//    }
+//    else
+//    {
+//        ledgeChangeAnim.enabled = false;
+//    }
+//}
+
+//private void Start()
+//{
+//    ledgeChangeAnim.enabled = false;
+//    playerGameObj = GameObject.FindGameObjectWithTag("Player");
+//}
+
+//private void Update()
+//{
+//    float timer = 4;
+
+//    //Check if player on other side
+//    if (ledgeChangeAnim.enabled == true)
+//    {
+//        timer = timer - 1;
+//        Debug.Log("Timer: " + timer);
+
+//        if (timer <= 0)
+//        {
+//            ledgeChangeAnim.enabled = false;
+//            timer = 4;
+//            //reset
+//            Debug.Log("Timer: " + timer + " Now at end");
+//        }
+//    }
+//}
