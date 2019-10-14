4d65f0d4bc8837f804bef3bbc7fd0834d3da29f0
SaRtHaK-Sa-xena <34013565+SaRtHaK-Sa-xena@users.noreply.github.com>
2019-10-14 22:55:53 +1100

    Saving For Tomorrow
    Need to fix the prefab transformed afterwards.

--- UNITY/UnityToonShader-skeleton/Assets/Scripts/detach.cs
+++ UNITY/UnityToonShader-skeleton/Assets/Scripts/detach.cs
@@ -6,22 +6,40 @@ public class detach : MonoBehaviour
 {
     public Animator myAnim;
 
+    public GameObject leftToRight;
+    public GameObject rightToLeft;
 
-    public Transform SpawnPointLeft;
-    public Transform SpawnPointRight;
+    public GameObject leftToRight_pref;
+    public GameObject rightToLeft_pref;
 
-    public GameObject ledgeLeft;
-    public GameObject leftPrefab;
-
-    public GameObject ledgeRight;
-    public GameObject rightPrefab;
+    public Transform leftToRight_spawn;
+    public Transform rightToLeft_spawn;
 
 
-    private void destroyLeft()
+    private void DestroyLeftToRight()
     {
-        Destroy(ledgeLeft);
-
+        Destroy(leftToRight);
     }
+
+
+    private void DestroyRightToLeft()
+    {
+        Destroy(rightToLeft);
+    }
+
+
+    private void Reposition()
+    {
+        if(leftToRight == null)
+        {
+            Instantiate(leftToRight_pref, leftToRight_spawn.transform);
+        }
+        if (rightToLeft == null)
+        {
+            Instantiate(rightToLeft_pref, rightToLeft.transform);
+        }
+    }
+
 
     private void detachAllChildren()
     {
@@ -29,26 +47,10 @@ public class detach : MonoBehaviour
     }
 
 
-    private void Reposition()
-    {
-        //transform.position.Set(-0.4, 3.829999, -9.99);
-        //transform.SetPositionAndRotation(Vector3(-0.4, 3.82999, -9.99), Quaternion.identity);
-
-        if(ledgeLeft == null)
-        {
-            Instantiate(leftPrefab, SpawnPointLeft.transform);
-        }
-        if(ledgeRight == null)
-        {
-            Instantiate(rightPrefab, SpawnPointRight.transform);
-        }
-
-    }
-
     // Start is called before the first frame update
     void Start()
     {
-        detachAllChildren();
+        myAnim.enabled = false;
     }
 
     // Update is called once per frame
@@ -57,3 +59,45 @@ public class detach : MonoBehaviour
         
     }
 }
+
+
+//public Animator myAnim;
+
+
+//public Transform SpawnPointLeft;
+//public Transform SpawnPointRight;
+
+//public GameObject ledgeLeft;
+//public GameObject leftPrefab;
+
+//public GameObject ledgeRight;
+//public GameObject rightPrefab;
+
+
+//private void destroyLeft()
+//{
+//    Destroy(ledgeLeft);
+
+//}
+
+//private void detachAllChildren()
+//{
+//    transform.DetachChildren();
+//}
+
+
+//private void Reposition()
+//{
+//    //transform.position.Set(-0.4, 3.829999, -9.99);
+//    //transform.SetPositionAndRotation(Vector3(-0.4, 3.82999, -9.99), Quaternion.identity);
+
+//    if (ledgeLeft == null)
+//    {
+//        Instantiate(leftPrefab, SpawnPointLeft.transform);
+//    }
+//    if (ledgeRight == null)
+//    {
+//        Instantiate(rightPrefab, SpawnPointRight.transform);
+//    }
+
+//}
