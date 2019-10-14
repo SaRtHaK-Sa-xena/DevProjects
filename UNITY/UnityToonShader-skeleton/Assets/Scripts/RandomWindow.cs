88847fcf7f6cdcf11f2e5c21838502a9ee2a4fd0
SaRtHaK-Sa-xena <34013565+SaRtHaK-Sa-xena@users.noreply.github.com>
2019-10-14 14:59:58 +1100

    Saving Changes
    The project now includes the working transition from one plane to another.

--- UNITY/UnityToonShader-skeleton/Assets/Scripts/RandomWindow.cs
+++ UNITY/UnityToonShader-skeleton/Assets/Scripts/RandomWindow.cs
@@ -1,4 +1,4 @@
-﻿.using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,7 +9,6 @@ public class RandomWindow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
 
     }
 
