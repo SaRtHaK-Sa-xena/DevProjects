df5487a2379aafc14b9e2a2055e6a30682547ac4
Sarthak Saxena <sarthaksaxena42@yahoo.com>
2019-10-28 16:12:38 +1100

    Saving
    This project is including the updtaed Mal game with Map. But the square doesn't change colour.

---
+++ UNITY/Grey Box Build V1-M/Mal's Life grey box/Assets/Scripts/TestingColourFormating.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingColourFormating : MonoBehaviour
+{
+    public GameObject testingItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        testingItem.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- UNITY/Grey Box Build V1-M/Mal's Life grey box/Assets/Scripts/itemFunctionOnPickUp.cs
+++ UNITY/Grey Box Build V1-M/Mal's Life grey box/Assets/Scripts/itemFunctionOnPickUp.cs
@@ -6,7 +6,7 @@ public class itemFunctionOnPickUp : MonoBehaviour
 {
     public GameObject playerObj;
     public GameObject playeritemHolder;
-    public GameObject parentOfItem;
+    //public GameObject parentOfItem;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,6 +16,8 @@ public class itemFunctionOnPickUp : MonoBehaviour
         //If the player is not currently holding an item
         if (playerObj.GetComponent<itemManager>().HoldingItem == false)
         {
+            gameObject.GetComponentInParent<itemOnPosition>().HasItem = false;
+
             //Remove Collider of item
             gameObject.GetComponent<BoxCollider>().enabled = false;
 
@@ -24,7 +26,7 @@ public class itemFunctionOnPickUp : MonoBehaviour
             gameObject.transform.SetPositionAndRotation(playeritemHolder.transform.position, Quaternion.identity);
             playerObj.GetComponent<itemManager>().HoldingItem = true;
         }
-        
+
     }
 
     // Start is called before the first frame update
@@ -41,3 +43,65 @@ public class itemFunctionOnPickUp : MonoBehaviour
         playerObj = GameObject.FindGameObjectWithTag("Player");
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class itemFunctionOnPickUp : MonoBehaviour
+//{
+//    public GameObject playerObj;
+//    public GameObject playeritemHolder;
+//    public GameObject parentOfItem;
+
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        playeritemHolder = GameObject.Find("itemHolder");
+//        playerObj = GameObject.FindGameObjectWithTag("Player");
+
+//        //If the player is not currently holding an item
+//        if (playerObj.GetComponent<itemManager>().HoldingItem == false)
+//        {
+//            //Remove Collider of item
+//            gameObject.GetComponent<BoxCollider>().enabled = false;
+
+//            //Set Parent of Item to Player
+//            gameObject.transform.SetParent(playeritemHolder.transform);
+//            gameObject.transform.SetPositionAndRotation(playeritemHolder.transform.position, Quaternion.identity);
+//            playerObj.GetComponent<itemManager>().HoldingItem = true;
+//        }
+
+//    }
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        playeritemHolder = GameObject.Find("itemHolder");
+//        playerObj = GameObject.FindGameObjectWithTag("Player");
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        playeritemHolder = GameObject.Find("itemHolder");
+//        playerObj = GameObject.FindGameObjectWithTag("Player");
+//    }
+//}
