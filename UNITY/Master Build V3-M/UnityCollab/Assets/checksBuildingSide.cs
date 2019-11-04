6d81cf194c8f0ec21f737160ac189d80c64d6267
Sarthak Saxena <sarthaksaxena42@yahoo.com>
2019-11-04 14:34:12 +1100

    Saving Prototype
    Need to fix the load on collision. Update TDD.

---
+++ UNITY/Master Build V3-M/UnityCollab/Assets/checksBuildingSide.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checksBuildingSide : MonoBehaviour
+{
+    public float value;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("collidorFront"))
+        {
+            value = 0;
+        }
+        if (other.CompareTag("collidorLeft"))
+        {
+            value = 90;
+        }
+        if (other.CompareTag("collidorRight"))
+        {
+            value = -90;
+        }
+        if (other.CompareTag("collidorBack"))
+        {
+            value = -180;
+        }
+    }
+}
