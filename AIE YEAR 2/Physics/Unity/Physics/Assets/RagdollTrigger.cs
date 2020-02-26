9c4cfe3d41ee2ebac32c126bee6605f016c44df8
Sarthak Saxena <34013565+SaRtHaK-Sa-xena@users.noreply.github.com>
2020-02-26 14:55:42 +1100

    Update To Github
    Adding Unity Project including Physics
    This project will be a FPS physics game.

---
+++ AIE YEAR 2/Physics/Unity/Physics/Assets/RagdollTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Ragdoll ragdoll = other.gameObject.GetComponentInParent<Ragdoll>();
+        if(ragdoll != null)
+        {
+            ragdoll.RagdollOn = true;
+        }
+    }
+}
---
+++ AIE YEAR 2/Physics/Unity/Physics/Assets/Scripts/CannonBall.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class CannonBall : MonoBehaviour
+{
+    public float forceOnFire = 9000;
+
+    bool fire = false;
+    bool canFire = true;
+
+    Rigidbody rb = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown && canFire)
+        {
+            rb.isKinematic = false;
+            rb.AddForce(transform.forward * forceOnFire);
+            canFire = false;
+        }
+    }
+}
