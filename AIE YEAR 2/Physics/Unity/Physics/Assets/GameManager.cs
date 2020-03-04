754c3a50f2f276c769357a7ed79588be60572a19
Sarthak Saxena <34013565+SaRtHaK-Sa-xena@users.noreply.github.com>
2020-03-04 16:52:50 +1100

    Update
    Saving before end of day.

--- AIE YEAR 2/Physics/Unity/Physics/Assets/GameManager.cs
+++ AIE YEAR 2/Physics/Unity/Physics/Assets/GameManager.cs
@@ -21,5 +21,7 @@ public class GameManager : MonoBehaviour
         {
             Cursor.lockState = CursorLockMode.None;
         }
+
+        //GameObject[] listOfEnemies = GameObject.FindGameObjectsWithTag("")
     }
 }
